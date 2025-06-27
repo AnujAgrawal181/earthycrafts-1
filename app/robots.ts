a2cd84ccce3fae5bde0dd42c5bdb490f323fc7e8
Monika Mathur <monika.mathur1911@gmail.com>
2025-06-27 22:10:55 +0530

    test

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,7 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://earthycrafts.com";
 
+  
   return {
     rules: [
       {

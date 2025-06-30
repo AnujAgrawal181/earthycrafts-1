8f4a282dd309798ba487dcc913a07f5958c82a72
Monika Mathur <monika.mathur1911@gmail.com>
2025-07-01 01:38:50 +0530

    removed unused components

--- app/(routes)/(main)/home/page.tsx
+++ app/(routes)/(main)/home/page.tsx
@@ -1,10 +1,8 @@
 import CategorySection from "@/components/home-page/category-section";
 import CustomizeSection from "@/components/home-page/customize-section";
-import GalleryGrid from "@/components/home-page/gallery-grid";
 import HeroSection from "@/components/home-page/hero-section";
 import ProductSlide2 from "@/components/home-page/product-slide2";
 import SubCategorySection from "@/components/home-page/sub-category-section";
-import YourSpace from "@/components/home-page/your-space-section";
 
 export default async function Home() {
   debugger
